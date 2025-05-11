f64441aa73a95480cc7d277216edeb8ad2d07c94
sammmmtomas <yutthaporn.svr@gmail.com>
2025-05-12 06:23:57 +0700

    Add files via upload

--- build.js
+++ build.js
@@ -1,15 +1,12 @@
-// build.js
 import { build } from "esbuild";
 import { mkdir, copyFile } from "fs/promises";
 import { rimraf } from "rimraf";
-import path from "path";
 
 const isDev = process.argv.includes("--dev");
 
 await rimraf("dist");
 await mkdir("dist", { recursive: true });
 
-// copy static config files
 await copyFile("src/uv.config.js", "dist/uv.config.js");
 await copyFile("src/uv.sw.js", "dist/uv.sw.js");
 await copyFile("src/index.html", "dist/index.html");
