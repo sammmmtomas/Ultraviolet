b0e906690e2d01e6661986152dee273324aa4215
sammmmtomas <yutthaporn.svr@gmail.com>
2025-05-12 04:36:10 +0700

    Update build.js

--- build.js
+++ build.js
@@ -7,8 +7,8 @@ const isDev = process.argv.includes("--dev");
 await rimraf("dist");
 await mkdir("dist", { recursive: true });
 
-// copy static files
-await copyFile("src/uv.sw.js", "dist/sw.js");
+// Copy static files
+await copyFile("src/uv.sw.js", "dist/uv.sw.js");
 await copyFile("src/uv.config.js", "dist/uv.config.js");
 
 await build({
@@ -16,16 +16,25 @@ await build({
     "uv.bundle": "./src/rewrite/index.js",
     "uv.client": "./src/client/index.js",
     "uv.handler": "./src/uv.handler.js",
-    "uv.sw": "./src/uv.sw.js",
+    "uv.sw": "./src/uv.sw.js"
   },
   bundle: true,
   minify: !isDev,
   sourcemap: isDev,
   outdir: "dist",
   define: {
-    "process.env.NODE_ENV": JSON.stringify(isDev ? "development" : "production"),
+    "process.env.NODE_ENV": JSON.stringify(isDev ? "development" : "production")
   },
   target: "esnext",
   platform: "browser",
   logLevel: "info",
+  external: [
+    "events",
+    "set-cookie-parser",
+    "idb",
+    "parse5",
+    "meriyah",
+    "astring",
+    "@mercuryworkshop/bare-mux"
+  ]
 });
