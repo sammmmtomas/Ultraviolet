3fa86db2bf7bb1ae872c7cb4fded68f7b6269194
sammmmtomas <yutthaporn.svr@gmail.com>
2025-05-12 04:18:57 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,22 +3,18 @@ import { readFile } from "fs/promises";
 import path from "path";
 import { fileURLToPath } from "url";
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
-const port = process.env.PORT || 8080;
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-app.use("/uv.sw.js", express.static(path.join(__dirname, "dist", "uv.sw.js")));
-app.use("/uv.bundle.js", express.static(path.join(__dirname, "dist", "uv.bundle.js")));
-app.use("/uv.handler.js", express.static(path.join(__dirname, "dist", "uv.handler.js")));
-app.use("/uv.client.js", express.static(path.join(__dirname, "dist", "uv.client.js")));
-app.use("/uv.config.js", express.static(path.join(__dirname, "dist", "uv.config.js")));
-app.use("/static", express.static(path.join(__dirname, "static")));
+app.use(express.static(path.join(__dirname, "dist")));
 
-app.get("/", async (req, res) => {
-  const html = await readFile(path.join(__dirname, "static", "index.html"), "utf8");
+app.get("/service/:path(*)", async (req, res) => {
+  const html = await readFile(path.join(__dirname, "dist", "index.html"), "utf-8");
+  res.setHeader("Content-Type", "text/html");
   res.send(html);
 });
 
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
-  console.log(`✅ Ultraviolet running on port ${port}`);
+  console.log(`✅ Server running on http://localhost:${port}`);
 });
