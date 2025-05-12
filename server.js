829e3c58537757ba1f6a36abf77a1d5b7804e64e
sammmmtomas <yutthaporn.svr@gmail.com>
2025-05-12 15:57:50 +0000

    chore: rebuild dist after fixing server.js

--- server.js
+++ server.js
@@ -20,5 +20,5 @@ app.get("*", (_, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`✅ Server is running on http://localhost:${PORT}`);
+   console.log(`✅ Server is running on port ${PORT}`);
 });
