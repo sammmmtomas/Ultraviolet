e03beabb22b1b929315b20286899af1ab7a1e2e7
sammmmtomas <yutthaporn.svr@gmail.com>
2025-05-12 17:16:28 +0000

    fix: add index.html to dist

--- build.js
+++ build.js
@@ -7,13 +7,17 @@ const isDev = process.argv.includes("--dev");
 await rimraf("dist");
 await mkdir("dist", { recursive: true });
 
+// copy static files
+await copyFile("src/index.html", "dist/index.html");
 await copyFile("src/uv.config.js", "dist/uv.config.js");
 await copyFile("src/uv.sw.js", "dist/uv.sw.js");
 
+// build js bundles
 await build({
   entryPoints: {
     "uv.bundle": "./src/uv.bundle.js",
-    "uv.client": "./src/uv.client.js"
+    "uv.client": "./src/uv.client.js",
+    "uv.handler": "./src/uv.handler.js",
   },
   bundle: true,
   minify: !isDev,
@@ -21,13 +25,21 @@ await build({
   outdir: "dist",
   platform: "browser",
   target: "esnext",
-  logLevel: "info",
   define: {
     "process.env.NODE_ENV": JSON.stringify(isDev ? "development" : "production"),
   },
   footer: {
-    js: "self.Ultraviolet = Ultraviolet;"
-  }
+    js: "self.Ultraviolet = Ultraviolet;",
+  },
+  external: [
+    "events",
+    "idb",
+    "parse5",
+    "set-cookie-parser",
+    "astring",
+    "meriyah",
+    "@mercuryworkshop/bare-mux",
+  ],
 });
 
 console.log("✅ Build complete.");
